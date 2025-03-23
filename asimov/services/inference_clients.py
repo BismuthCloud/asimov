7962564d7fdb92ce26606314714033fac4385d5d
Ian Butler <itb206@nyu.edu>
2025-03-23 13:48:38 -0700

    Setup asimov for generic structured parsing

--- asimov/services/inference_clients.py
+++ asimov/services/inference_clients.py
@@ -177,11 +177,15 @@ class InferenceClient(ABC):
             ]
         ] = None,
         fifo_ratio: Optional[float] = None,
-        tool_parser = None
+        tool_parser = None,
+        tool_result_reducer: Callable[list[dict[str, Any]], str] = None
     ):
         mode = None
         if mode_swap_callback:
             _, _, mode = await mode_swap_callback()
+
+        if tool_parser and not tool_result_reducer:
+            raise ValueError("If tool_parser is set then tool_result_reducer must be set.")
 
         last_mode_cached_message: dict[Hashable, int] = {}
 
@@ -299,7 +303,7 @@ class InferenceClient(ABC):
                         if not resp:
                             raise InferenceException("no response blocks returned")
                         
-                        calls = tool_parser(resp[-1]["text"])
+                        calls = await tool_parser(resp[-1]["text"])
 
                         if type(calls) is not list:
                             calls = [calls]
@@ -361,17 +365,13 @@ class InferenceClient(ABC):
                 try:
                     func = tool_funcs.get(call["name"])
 
-                    print(call["name"])
-
-                    print(func)
-
                     if not func:
                         result = "This tool is not available please select and use an available tool."
                     else:
                         result = await func(call["input"])
                 except StopAsyncIteration:
                     return serialized_messages
-                
+                 
                 if tool_parser:
                     content_blocks.append(
                         {
@@ -387,6 +387,11 @@ class InferenceClient(ABC):
                             "content": str(result),
                         }
                     )
+
+            if tool_parser:
+                content_blocks = [
+                    {"type": "text", "content": await tool_result_reducer(content_blocks)}
+                ]
 
             serialized_messages.append(
                 {
@@ -1756,8 +1761,6 @@ class OpenRouterInferenceClient(OAIInferenceClient):
                 ),
                 None,
             )
-            if not text.get("text"):
-                print(text)
             message = {
                 "role": message["role"],
                 "content": text.get("content") or text.get("text"),
